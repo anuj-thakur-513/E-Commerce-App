04c8063ed52a5bacf9fb0798289e111ce203b5f4
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-05-20 02:09:43 +0530

    get JSON data from server and display

--- Android/app/src/main/java/com/example/e_commerce/ShowJSONObjectActivity.kt
+++ Android/app/src/main/java/com/example/e_commerce/ShowJSONObjectActivity.kt
@@ -3,6 +3,11 @@ package com.example.e_commerce
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_show_jsonobject.*
 
 class ShowJSONObjectActivity : AppCompatActivity() {
@@ -11,7 +16,21 @@ class ShowJSONObjectActivity : AppCompatActivity() {
         setContentView(R.layout.activity_show_jsonobject)
 
         btnSubmit.setOnClickListener(View.OnClickListener { view: View? ->
-            
+            val productURL = "http://192.168.179.48/ECommerceApp/present_json.php?id=" + edtProductID.text.toString()
+
+            val requestQ: RequestQueue = Volley.newRequestQueue(this@ShowJSONObjectActivity)
+            val jsonObjectRequest: JsonObjectRequest = JsonObjectRequest(Request.Method.GET, productURL,
+                null,
+                { response ->
+                    txtProductName.text = response.getString("name")
+                    txtProductPrice.text = response.getInt("price").toString()
+                },
+                { error ->
+                    txtProductName.text = "No product found"
+                    txtProductPrice.text = "No product found"
+                })
+
+            requestQ.add(jsonObjectRequest)
         })
     }
 }
