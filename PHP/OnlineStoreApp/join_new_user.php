99eaa6fce48bf9fc81497c1184ef1bc058c4284e
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-05-28 02:16:40 +0530

    adding users with unique email address to the database using PHP

---
+++ PHP/OnlineStoreApp/join_new_user.php
@@ -0,0 +1,23 @@
+<?php
+
+$connection = new mysqli("localhost","root","","online_store_db");
+
+
+//$sqlCommand = $connection->prepare("insert into app_users_table values (?,?,?)");
+//$sqlCommand->bind_param("sss", $_GET["email"],$_GET["username"],$_GET["pass"]);
+//$sqlCommand->execute();
+
+// If an email already exists, do not add the user again
+$emailCheckSQLCommand = $connection->prepare("select * from app_users_table where email=?");
+$emailCheckSQLCommand->bind_param("s",$_GET["email"]);
+$emailCheckSQLCommand->execute();
+$emailResult = $emailCheckSQLCommand->get_result();
+
+
+if ($emailResult->num_rows == 0) {
+    $sqlCommand = $connection->prepare("insert into app_users_table values(?,?,?)");
+    $sqlCommand->bind_param("sss", $_GET["email"], $_GET["username"], $_GET["pass"]);
+    $sqlCommand->execute();
+} else {
+    echo "A user with this e-mail address already exists";
+}
