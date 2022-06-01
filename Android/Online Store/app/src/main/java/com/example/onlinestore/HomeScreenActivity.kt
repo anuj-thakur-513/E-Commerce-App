f87eff57a6497097c5c134f46a8b4da42a21b2c9
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-06-01 20:40:33 +0530

    getting the brands and showing on ListView

--- Android/Online Store/app/src/main/java/com/example/onlinestore/HomeScreenActivity.kt
+++ Android/Online Store/app/src/main/java/com/example/onlinestore/HomeScreenActivity.kt
@@ -1,11 +1,45 @@
 package com.example.onlinestore
 
+import android.app.DownloadManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AlertDialog
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_home_screen.*
 
 class HomeScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+        val IP_ADDRESS = "192.168.1.5"
+        val brandsURL = "http://$IP_ADDRESS/OnlineStoreApp/fetch_brands.php"
+
+        var brandsList = ArrayList<String>()
+
+        // Creating a request queue in order to fetch the JSON data
+        var requestQ = Volley.newRequestQueue(this@HomeScreenActivity)
+        var jsonAR = JsonArrayRequest(Request.Method.GET, brandsURL, null, { response ->
+
+            for (jsonObject in 0.until(response.length())) {
+                brandsList.add(response.getJSONObject(jsonObject).getString("brand"))
+            }
+
+            var brandsListAdapter = ArrayAdapter(this@HomeScreenActivity, android.R.layout.simple_list_item_1, brandsList)
+            brandsListView.adapter = brandsListAdapter
+
+        }, { error ->
+            // Creating an Alert Dialog
+            val dialogBuilder = AlertDialog.Builder(this)
+            dialogBuilder.setTitle("Message")
+            dialogBuilder.setMessage(error.message)
+            dialogBuilder.create().show()
+        })
+
+        requestQ.add(jsonAR)
     }
 }
