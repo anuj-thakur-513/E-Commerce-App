17418dd8171ff9632f18a256c006c9d0abea5db4
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-06-02 23:58:23 +0530

    PHP get electronic products

---
+++ PHP/OnlineStoreApp/fetch_eproducts.php
@@ -0,0 +1,15 @@
+<?php
+
+$connection = new mysqli("localhost","root","", "online_store_db");
+$fetchProductsCommand = $connection->prepare("select * from electronic_products_table where brand=?");
+$fetchProductsCommand->bind_param("s", $_GET["brand"]);
+$fetchProductsCommand->execute();
+
+$epResults = $fetchProductsCommand->get_result();
+$epArray = array();
+
+while ($row = $epResults->fetch_assoc()){
+    array_push($epArray, $row);
+}
+
+echo json_encode($epArray);
