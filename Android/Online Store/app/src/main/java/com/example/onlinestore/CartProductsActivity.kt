2e7ddf65c0a22453231055c74aec0bfb9d1958b9
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-06-05 22:21:26 +0530

    Android get temporary orders as JSON objects

--- Android/Online Store/app/src/main/java/com/example/onlinestore/CartProductsActivity.kt
+++ Android/Online Store/app/src/main/java/com/example/onlinestore/CartProductsActivity.kt
@@ -2,10 +2,38 @@ package com.example.onlinestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_cart_products.*
 
 class CartProductsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart_products)
+
+        val IP_ADDRESS = "192.168.1.6"
+        var cartProductsUrl = "http://$IP_ADDRESS/OnlineStoreApp/fetch_temporary_order.php?email=${Person.email}"
+        var cartProductsList = ArrayList<String>()
+        var requestQ = Volley.newRequestQueue(this@CartProductsActivity)
+        var jsonAR = JsonArrayRequest(Request.Method.GET, cartProductsUrl, null, {response->
+
+            for (joIndex in 0.until(response.length())){
+
+                cartProductsList.add("${response.getJSONObject(joIndex).getString("name")} \n" +
+                        "${response.getJSONObject(joIndex).getInt("price")} \n" +
+                        "${response.getJSONObject(joIndex).getInt("amount")}")
+
+            }
+
+            var cartProductsAdapter = ArrayAdapter(this@CartProductsActivity, android.R.layout.simple_list_item_1, cartProductsList)
+            cartProductsListView.adapter = cartProductsAdapter
+
+        }, {error->
+
+        })
+
+        requestQ.add(jsonAR)
     }
 }
