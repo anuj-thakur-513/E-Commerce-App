4cd1df75960953374e963be2f12207476e131447
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-06-06 15:40:11 +0530

    Android get the total price from the server

--- Android/Online Store/app/src/main/java/com/example/onlinestore/FinalizeShoppingActivity.kt
+++ Android/Online Store/app/src/main/java/com/example/onlinestore/FinalizeShoppingActivity.kt
@@ -2,10 +2,29 @@ package com.example.onlinestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_finalize_shopping.*
 
 class FinalizeShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finalize_shopping)
+
+        //TODO: Keep updating the IP ADDRESS accordingly
+        val IP_ADDRESS = "192.168.1.6"
+        var calculateTotalPriceUrl = "http://$IP_ADDRESS/OnlineStoreApp/calculate_total_price.php?" +
+                "invoice_num=${intent.getStringExtra("LATEST_INVOICE_NUMBER")}"
+
+        var requestQ = Volley.newRequestQueue(this@FinalizeShoppingActivity)
+        var stringRequest = StringRequest(Request.Method.GET, calculateTotalPriceUrl, {response->
+
+           btnPaymentProcessing.text = "Pay $$response via PayPal"
+
+        }, {error->
+
+        })
+        requestQ.add(stringRequest)
     }
 }
